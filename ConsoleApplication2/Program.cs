33fa1e974139b72f6a1dda61b7d1b144b3afb183
elfenliedjp <telescopion@hotmail.co.jp>
2014-12-30 21:36:12 +0900

    デフォルト修飾子のコメントをいれた

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -8,9 +8,46 @@ namespace ConsoleApplication2
 {
     class Program
     {
+        /**
+         * テスト定数
+         */
+        public const String TEST_CONST = "テストだよ！";
+
+        /**
+         * 
+         */
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Sand sand = new Sand();
+            sand.kind = "すごい";
+            sand.g = 10000;
+            sand.playSand();
+            a.output();
+
+        }
+
+        void output()
+        {
             Console.Read();
+        }
+    }
+
+    class Sand
+    {
+        /**
+         * ※ メソッド/メンバ変数はアクセス修飾子を省略するとprivateになる！！！！
+         */
+
+        // 砂の種類
+        String kind = "ふつう";
+        // グラム
+        public int g = 0;
+
+
+        internal void playSand()
+        {
+            Console.WriteLine("{0}の砂を{1}グラム使って遊んだ！", kind, g);
         }
     }
 }
