850009c86272de0bcf2e681cdf4c752e70f07d38
elfenliedjp <telescopion@hotmail.co.jp>
2014-12-30 22:04:11 +0900

    こんすとらくた

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -24,7 +24,7 @@ namespace ConsoleApplication2
             sand.g = 10000;
             sand.playSand();
 
-            UltimateSand uSand = new UltimateSand();
+            UltimateSand uSand = new UltimateSand(0);
             uSand.kind = "ヤバい";
             uSand.g = 10;
             uSand.mass = 10000;
@@ -40,33 +40,46 @@ namespace ConsoleApplication2
         }
     }
 
-    class UltimateSand : Sand
-    {
-        // 砂の質量
-        public int mass = 0;
-
-        internal void playSand()
-        {
-            Console.WriteLine("{0}の砂を{1}グラム使って遊んだ！", kind, g);
-            Console.WriteLine("さらに質量を{0}に変えた！", mass);
-        }
-    }
-
     class Sand
     {
         /**
          * ※ メソッド/メンバ変数はアクセス修飾子を省略するとprivateになる！！！！
          */
-
         // 砂の種類
         public String kind = "ふつう";
         // グラム
         public int g = 0;
 
 
-        internal void playSand()
+        /**
+         * オーバーライド元はvirtual
+         */ 
+        public virtual void playSand()
         {
             Console.WriteLine("{0}の砂を{1}グラム使って遊んだ！", kind, g);
+        }
+    }
+
+    class UltimateSand : Sand
+    {
+        /**
+         * コンストラクタのスコープって・・・
+         */
+        public UltimateSand(int mass)
+        {
+            this.mass = mass;
+        }
+
+        // 砂の質量
+        public int mass = 0;
+
+        /**
+         * オーバーライド先はnewかoverride
+         */
+        public new void playSand()
+        {
+            Console.WriteLine("{0}の砂を{1}グラム使って遊んだ！", kind, g);
+            Console.WriteLine("さらに質量を{0}に変えた！", mass);
         }
     }
 }
